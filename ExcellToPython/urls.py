3b0e7efb20c3258dacaa967e9591952bc1ca9dd4
bulcedoc <115892291+bulcedoc@users.noreply.github.com>
2023-01-09 16:16:51 +0530

    Add files via upload

---
+++ ExcellToPython/urls.py
@@ -0,0 +1,32 @@
+"""ExcellToPython URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('store/', store , name="store"),
+    path('student/', student , name="student"),
+    path('shop/', shop , name="shop"),
+    path('col/', col, name="col"),
+    path('checkout/', checkout, name="ch"),
+    path('coll_c/', col_c, name="cl-c"),
+    path('sh/', shop_c, name="sh-c"),
+    path('sto/', store_c, name="sto-c"),
+    path('stu/', stu_c, name="stu-c"),
+]
---
+++ ExcellToPython/views.py
@@ -0,0 +1,208 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+cid = []
+cname = []
+cadd = []
+cph = []
+cemail = []
+cdn = []
+cdh =[]
+shcustnid = []
+shcustname = []
+shcustnum = []
+shemail = []
+shStocknid =[]
+shStockdesp = []
+shstocktyp =[]
+shpayid = []
+shpaydate = []
+shpaydesp = []
+stsalesnid = []
+stSdesp = []
+stSamount = []
+stsalesname =[]
+stprodnum = []
+stprodesp=[]
+stprotyp=[]
+stprodit=[]
+stpayid=[]
+stpaydate=[]
+stpaydesp=[]
+studnid = []
+studname = []
+studadd = []
+stuEmail=[]
+studphnum=[]
+studdept =[]
+Studdob=[]
+# Create your views here.
+def home(request):
+    return render(request,'test.html')
+
+def col(request):
+    if request.method == 'POST':
+        cid.append(request.POST.get('clgnid'))
+        cname.append(request.POST.get('clgname'))
+        cadd.append(request.POST.get('clgadd'))
+        cph.append(request.POST.get('clgphnum'))
+        cemail.append(request.POST.get('clgEmail'))
+        cdn.append(request.POST.get('depname'))
+        cdh.append(request.POST.get('depthead'))
+        return render(request,'college.html')   
+    return render(request,'college.html')
+def col_c(request):
+ import openpyxl
+ from openpyxl import Workbook
+ res = HttpResponse(content_type= 'application/ms-excel')
+ res['Content-Disposition'] = 'attachment; filename= "college.xls"'
+ wb = Workbook() 
+ sheet = wb.active 
+ sheet['A1'] = " College ID " 
+ sheet['B1'] = " College name " 
+ sheet['C1'] = " College Address " 
+ sheet['D1'] = " College Contact " 
+ sheet['E1'] =" College Email " 
+ sheet['F1'] =" Department Name " 
+ sheet['G1'] =" Department Head "
+ data =[]
+ i= 0
+ while i<(len(cid)):
+  d = (cid[i],cname[i],cadd[i],cph[i],cemail[i],cdn[i],cdh[i])
+  data.append(d)
+  print(d)
+  i = i+1
+ for i in data: 
+    sheet.append(i) 
+ wb.save(res) 
+
+ return res
+
+
+def shop(request):
+    if request.method == 'POST':
+        shcustnid.append(request.POST.get('custnid'))
+        shcustname.append(request.POST.get('custname'))
+        shcustnum.append(request.POST.get('custnum'))
+        shemail.append(request.POST.get('email'))
+        shStocknid.append(request.POST.get('Stocknid'))
+        shStockdesp.append(request.POST.get('Stockdesp'))
+        shstocktyp.append(request.POST.get('stocktyp'))
+        shpayid.append(request.POST.get('payid'))
+        shpaydate.append(request.POST.get('paydate'))
+        shpaydesp.append(request.POST.get('paydesp'))
+        return render(request,'shop.html')
+    return render(request,'shop.html')
+def shop_c(request):
+ import openpyxl
+ from openpyxl import Workbook
+ res = HttpResponse(content_type= 'application/ms-excel')
+ res['Content-Disposition'] = 'attachment; filename= "shop.xls"'
+ wb = Workbook() 
+ sheet = wb.active 
+ sheet['A1'] = " CustID " 
+ sheet['B1'] = " CustName " 
+ sheet['C1'] = " Cust Phone number " 
+ sheet['D1'] = " Cust Email " 
+ sheet['E1'] =" StockID " 
+ sheet['F1'] =" StockDescription " 
+ sheet['G1'] =" StockType "
+ sheet['H1'] = " Payment ID " 
+ sheet['I1'] =" Payment Date " 
+ sheet['J1'] =" Payment Description " 
+ data =[]
+ i= 0
+ while i<(len(shcustnid)):
+  d = (shcustnid[i],shcustname[i],shcustnum[i],shemail[i],shStocknid[i],shStockdesp[i],shstocktyp[i],shpayid[i],shpaydate[i],shpaydesp[i])
+  data.append(d)
+  i = i+1
+ for i in data: 
+    sheet.append(i) 
+ wb.save(res) 
+
+ return res
+
+def store(request):
+    if request.method == 'POST':
+        stsalesnid.append(request.POST.get('salesnid'))
+        stsalesname.append(request.POST.get('salesname'))
+        stSdesp.append(request.POST.get('Sdesp'))
+        stSamount.append(request.POST.get('Samount'))
+        stprodnum.append(request.POST.get('prodnum'))
+        stprodesp.append(request.POST.get('prodesp'))
+        stprotyp.append(request.POST.get('protyp'))
+        stprodit.append(request.POST.get('prodit'))
+        stpayid.append(request.POST.get('payid'))
+        stpaydate.append(request.POST.get('paydate'))
+        stpaydesp.append(request.POST.get('paydesp'))
+        
+        return render(request,'store.html')
+    return render(request,'store.html')
+def store_c(request):
+ import openpyxl
+ from openpyxl import Workbook
+ res = HttpResponse(content_type= 'application/ms-excel')
+ res['Content-Disposition'] = 'attachment; filename= "store.xls"'
+ wb = Workbook() 
+ sheet = wb.active 
+ sheet['A1'] = " Sales ID " 
+ sheet['B1'] = " Sales Type " 
+ sheet['C1'] = " Sales Description " 
+ sheet['D1'] = " Sales Amount " 
+ sheet['E1'] =" Product Number " 
+ sheet['F1'] =" Product Description " 
+ sheet['G1'] =" Product Type "
+ sheet['H1'] = " Product Items " 
+ sheet['I1'] =" Payment ID " 
+ sheet['J1'] =" Payment Date " 
+ sheet['K1'] =" Payment Description "
+ data =[]
+ i= 0
+ while i<(len(stsalesnid)):
+  d = (stsalesnid[i],stSdesp[i],stSamount[i],stsalesname[i],stprodnum[i],stprodesp[i],stprotyp[i],stprodit[i],stpayid[i],stpaydate[i],stpaydesp[i])
+  data.append(d)
+  i = i+1
+ for i in data: 
+    sheet.append(i) 
+ wb.save(res) 
+
+ return res
+
+def student(request):
+    if request.method == 'POST':
+        studnid.append(request.POST.get('studnid'))
+        studname.append(request.POST.get('studname'))
+        studadd.append(request.POST.get('studadd'))
+        Studdob.append(request.POST.get('Studdob'))
+        studdept.append(request.POST.get('studdept'))
+        studphnum.append(request.POST.get('studphnum'))
+        stuEmail.append(request.POST.get('Email'))
+        return render(request,'stu.html')
+    return render(request,'stu.html')
+def stu_c(request):
+ import openpyxl
+ from openpyxl import Workbook
+ res = HttpResponse(content_type= 'application/ms-excel')
+ res['Content-Disposition'] = 'attachment; filename= "student.xls"'
+ wb = Workbook() 
+ sheet = wb.active 
+ sheet['A1'] = " Student ID " 
+ sheet['B1'] = " Student name " 
+ sheet['C1'] = " Student Address " 
+ sheet['D1'] = " Student DOB " 
+ sheet['E1'] =" Student Department " 
+ sheet['F1'] =" Student Contact " 
+ sheet['G1'] =" Student Email "
+ data =[]
+ i= 0
+ while i<(len(studnid)):
+  d = (studnid[i],studname[i],studadd[i],Studdob[i],studdept[i],studphnum[i],stuEmail[i])
+  data.append(d)
+  i = i+1
+ for i in data: 
+    sheet.append(i) 
+ wb.save(res) 
+
+ return res
+
+def checkout(request):
+    return render(request,'form.html')
